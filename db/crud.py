e7f60136a584205dd0d23f3cbefb54574c85da9f
Husayn-Esmail <husayn@esmail.ca>
2022-05-03 13:24:02 -0600

    I think I've completed all of the database elements, at, interface with main hasn't been completed yet

--- db/crud.py
+++ db/crud.py
@@ -3,5 +3,17 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 def get_prev_query(db: Session, link: str, qstring: str, tag: str):
-    return db.query(modles.Prev_Query).filter(models.Prev_Query.link == link and models.Prev_Query.qstring == qstring).first()
+    return db.query(modles.Prev_Query).filter(models.Prev_Query.link == models.Prev_Query.qstring == qstring).first()
 
+def get_query_by_url(db: Session, link: str):
+    return db.query(models.Prev_Query).filter(models.Prev_Query.link == link).first()
+
+def get_prev_queries(db: Session, skip: int=0, limit: int =100):
+    return db.query(models.Prev_Query).offset(skip).limit(limit).all()
+
+def create_prev_query(db: Session, query: schemas.Prev_Query_Create):
+    db_prev_query = models.Prev_Query(link=Prev_Query.link, qstring = Prev_Query.qstring, tag = Prev_Query.tag)
+    db.add(db_prev_query)
+    db.commit()
+    db.refresh(db_prev_query)
+    return db_prev_query
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # This is connecting the database I think
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tagselector.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./db/tagselector.db"
 
 engine = create_engine (
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False}
