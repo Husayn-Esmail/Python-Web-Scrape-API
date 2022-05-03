75974ee2c30f52e91226d07c1c93e5b4c13b4366
Husayn-Esmail <husayn@esmail.ca>
2022-05-03 13:07:39 -0600

    completed the template and css for the API, functionality is not yet implemented yet

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
+# fast api things
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import Optional
 from pydantic import BaseModel
+# requests for handling get and post
 import requests
+# database things
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "splite:///./tagselector.db"
+# This is connecting the database I think
+SQLALCHEMY_DATABASE_URL = "sqlite:///./tagselector.db"
 
 engine = create_engine (
-    SQLALCEHMY_DATABASE_URL, connect_args={"check_same_thread":False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# this is handling the static files (like css)
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"),name="static")
+# this tells fastapi where to find templates
 templates = Jinja2Templates(directory="templates")
 
+# this is an object representing what I want to store in the database
 class Data(BaseModel):
     link: str
     text: str
     tag: str
 
-
+# this will serve my form template at the specified path
 @app.get("/form")
 async def form_post(request: Request):
     result= ""
     return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
 
+# this is supposed to handle form submissions but at the moment is not working
 @app.post("/form")
 async def form_post(request: Request, item: Data,  site: str = Form(...), qstring: str = Form(...)):
     item.link = site
     item.text = qstring
-    # return templates.TemplateResponse('form.html', context= {"request":request, 'result': result})
-    return item
+    return templates.TemplateResponse('form.html', context= {"request":request, 'result': result})
+    # return item
 
+# serves a page at the root directory. doesn't do anything right now
 @app.get('/')
 def read_root():
     return {"message":"Hello World"}
