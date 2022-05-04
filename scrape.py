d786956b6b0d638a29f938bdf1b9642cff636b0f
Husayn-Esmail <husayn@esmail.ca>
2022-05-04 01:23:14 -0600

    I think I'm getting somewhere with trying to find a string in the html while still maintaining the tag it came from. Haven't solved the input problem yet though. too tired to continue working tonight

--- scrape.py
+++ scrape.py
@@ -1,23 +1,90 @@
 import requests
 import re
-r = requests.get('https://www.webscraper.io/test-sites/tables')
 from bs4 import BeautifulSoup
-print(r)
+from html.parser import HTMLParser
+import lxml
 
-soup = BeautifulSoup(r.content, 'html.parser')
-longstring = soup.prettify()
+# the webpage that I'm scraping at the moment (converted to text)
+r = requests.get('https://www.studytonight.com/python/web-scraping/find-tags-with-beautifulsoup').text
+soup = BeautifulSoup(r, 'html.parser')
 
-divided = longstring.split('\n')
-# This finds the string, now how do we get what's around it?
+# selecting the head tag
+html_tag = soup.html
+# print(html_tag.contents)
+# this prints the tag and it's contents and then just the contents alone. i.e. there could be many duplicates of content.
+phrase = re.compile("learn", re.IGNORECASE)
 the_found = []
-count = 0
-for divide in divided:
-    print(divide,',')
-    j = re.findall("larry",divide.lower())
-    if j != []:
-        the_found.append(divide)
-    count += 1
+for child in html_tag.descendants:
+    try:
+        x = child.find_all(string=phrase)
+        if x != []:
+            the_found.append(x)
+    except:
+        if (str(child.string).__contains__(phrase)):
+            the_found.append(child)
+    # print(child)
+
+print(the_found)
+
+# the given string to be matched
 
 
-if the_found != []:
-    print("THE LAST ONE: ", the_found[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+# this just returns all text that are contained in the <p>
+# s = re.compile("the")
+# regex = "<[a-zA-Z]>+[a-zA-Z]+python+[a-zA-Z]+</[a-zA-Z]>"
+# texts = soup.find_all(string=s)
+# print(texts)
+# texts = soup.find_all(string=regex)
+# print(texts)
+# start_tags = []
+# end_tags = []
+# data = []
+
+
+# class MyHTMLParser(HTMLParser):
+#     def handle_starttag(self, tag, attrs):
+#         print("Start: <", tag,">")
+#         return tag
+    
+#     def handle_endtag(self, tag):
+#         print("End: <", tag,">")
+#         return tag
+    
+#     def handle_data(self,data):
+#         print("Data: ", data)
+#         return data
+
+# parser = MyHTMLParser()
+# i = parser.feed(r)
+
+# print(found)
+# longstring = soup.prettify()
+
+# divided = longstring.split('\n')
+# # This finds the string, now how do we get what's around it?
+# the_found = []
+# count = 0
+# for divide in divided:
+#     print(divide,',')
+#     j = re.compile(r"  (\s([a-zA-Z]+\s)+)  ",re.IGNORECASE)
+#     x = j.match(divide)
+#     the_found.append(x)
+#     count += 1
+
+
+# if the_found != []:
+#     print("THE LAST ONE: ", the_found[-1])
+
