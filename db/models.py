f4112e502116b5657bef502421d51e1967cf7308
Husayn-Esmail <husayn@esmail.ca>
2022-05-04 19:40:54 -0600

    created a new database

--- db/models.py
+++ db/models.py
@@ -7,8 +7,7 @@ class PrevQuery(Base):
     # name of the table to use
     __tablename__ = "queries"
     # columns in the database table
-    id = Column(Integer, unique=True, index=True)
     link = Column(String, index=True)
     qstring = Column(String, primary_key=True, index=True)
-    tag = Column(String)
+    tag = Column(String, index=True)
 
